0b8e91866c0865422d22f7b10798c375392170f6
vijaykumar <vijay10022@gmail.com>
2020-04-28 22:36:56 +0530

    Updates

---
+++ precache-manifest.e3ee50b4b3ecb60bc3c7721cda780fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a059bb05c0cf8a308ae88e0770423ede",
+    "url": "/vijaykumar10022/myreact/index.html"
+  },
+  {
+    "revision": "85faccff23a7bebb6898",
+    "url": "/vijaykumar10022/myreact/static/css/main.e7839ae2.chunk.css"
+  },
+  {
+    "revision": "3b6cb57efa8e17265d90",
+    "url": "/vijaykumar10022/myreact/static/js/2.e9bf7747.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vijaykumar10022/myreact/static/js/2.e9bf7747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85faccff23a7bebb6898",
+    "url": "/vijaykumar10022/myreact/static/js/main.eacd75a6.chunk.js"
+  },
+  {
+    "revision": "dc37b7327b5f01bccfd0",
+    "url": "/vijaykumar10022/myreact/static/js/runtime-main.d9a0a187.js"
+  }
+]);
